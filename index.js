6dae4079d823bfbcb65152ef2ddd447c5eea1353
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-26 15:38:06 -0400

    validation functions

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('hello')
+const zip = document.getElementById("zip");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+
+
+email.addEventListener("input", (event) => {
+    
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+
+zip.addEventListener("input", (event) => {
+   
+  if (zip.validity.patternMismatch) {
+    zip.setCustomValidity("I am expecting an valid zip code!");
+  } else {
+    zip.setCustomValidity("");
+  }
+});
+
+password.addEventListener("input", (event) => {
+   
+    if (password.validity.patternMismatch) {
+      password.setCustomValidity("Password must contain eight characters including one uppercase letter, one lowercase letter, and one number or special character");
+    } else {
+      password.setCustomValidity("");
+    }
+  });
+  
+
+
+
+
